85a9ce0a357b7cfce742337548c8c7f751c48492
Henrik Bengtsson <hb@aroma-project.org>
2021-12-16 11:37:13 -0800

    FIX: GISTIC2 peak names read from file were not trimmed from whitespace

--- R/read_gistic2.R
+++ R/read_gistic2.R
@@ -1,10 +1,21 @@
 #' @importFrom utils file_test read.csv
 #' @export
-read_gistic2 <- function(file, path = ".") {
+read_gistic2 <- function(file, path = ".", trim = TRUE) {
   file <- file.path(path, file)
   stopifnot(file_test("-f", file))
   
   res <- read.csv(file)
+
+  ## Trim all fields
+  if (trim) {
+    for (field in colnames(res)) {
+      value <- res[[field]]
+      if (is.character(value)) {
+        value <- gsub("(^[[:space:]]+|[[:space:]]+$)", "", value)
+        res[[field]] <- value
+      }
+    }
+  }
   
   res
 }
